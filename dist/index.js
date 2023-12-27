3201486c34d435b505986426f78644f5b46fb19a
Alexandr Cocier <licoms1997@gmail.com>
2023-12-27 12:14:33 +0200

    Fix commit

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,26 @@
 "use strict";
-//----------
+//-----------
 // functions
-//----------
+//-----------
 function addTwoNumbers(a, b) {
     return a + b;
 }
-;
-const substractTwoNumbers = (a, b) => {
+const subtractTwoNumbers = (a, b) => {
     return a - b;
 };
-addTwoNumbers(2, 4);
-substractTwoNumbers(10, 7);
+// addTwoNumbers('2', 5)
+addTwoNumbers(3, 9);
+subtractTwoNumbers(10, 7);
 function addAllNumbers(items) {
     const total = items.reduce((a, c) => a + c, 0);
     console.log(total);
 }
-;
-addAllNumbers([5, 7, 9, 11, 3, 2, 1, 100]);
+addAllNumbers([5, 7, 9, 11, 3, 2, 1]);
 //-----------------------
-//  return type inference
+// return type inference
 //-----------------------
 function formatGreeting(name, greeting) {
     return `${greeting}, ${name}`;
 }
-const result = formatGreeting('mario', 'hello');
+// we get inference on return types, but not on argument types
+// type inference on return values does not enforce a return type
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
-//----------
+//-----------
 // functions
-//----------
+//-----------
 
-function addTwoNumbers(a:number, b:number): number {
-    return a + b;
-};
-
-const substractTwoNumbers = (a: number, b: number): number => {
-    return a - b;
-};
-
-addTwoNumbers(2, 4);
-substractTwoNumbers(10, 7);
-
-function addAllNumbers(items: number[]){
-    const total = items.reduce((a, c) => a + c, 0);
-
-    console.log(total);
-};
-
-addAllNumbers([5,7,9,11,3,2,1,100]);
-
-//-----------------------
-//  return type inference
-//-----------------------
-
-function formatGreeting(name: string, greeting: string): string {
-    return `${greeting}, ${name}`;
+function addTwoNumbers(a: number, b: number): number {
+	return a + b
 }
 
-const result = formatGreeting('mario', 'hello');
+const subtractTwoNumbers = (a: number, b: number): number => {
+	return a - b
+}
+
+// addTwoNumbers('2', 5)
+addTwoNumbers(3, 9)
+subtractTwoNumbers(10, 7)
+
+function addAllNumbers(items: number[]): void {
+	const total = items.reduce((a, c) => a + c, 0)
+	console.log(total)
+}
+
+addAllNumbers([5, 7, 9, 11, 3, 2, 1])
+
+//-----------------------
+// return type inference
+//-----------------------
+
+function formatGreeting(name: string, greeting: string) {
+	return `${greeting}, ${name}`
+}
+
+// we get inference on return types, but not on argument types
+// type inference on return values does not enforce a return type
