d0fd9e3176afc7d704c53626677f49e235a01761
Alexandr Cocier <licoms1997@gmail.com>
2023-12-27 13:13:35 +0200

    Union Types

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,23 @@
 "use strict";
 //-------------
-// type aliases
+// union types
 //-------------
-function getRandomColor() {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return [r, g, b];
+let someId;
+someId = 1;
+someId = '2';
+let email = null;
+email = 'mario@netninja.dev';
+email = null;
+let anotherId;
+// anotherId = undefined
+anotherId = '1';
+anotherId = 2;
+//--------------------
+// union type pitfall
+//--------------------
+function swapIdType(id) {
+    // can only use props and methods common to
+    // both number and string types
+    // parseInt(id) --> not allowed
+    return id;
 }
-const colorOne = getRandomColor();
-const colorTwo = getRandomColor();
-console.log(colorOne, colorTwo);
-const userOne = {
-    name: 'Mario',
-    score: 75
-};
-function formatUser(user) {
-    console.log(`${user.name} has a score of ${user.score}`);
-}
-formatUser(userOne);
-formatUser({ name: 'Yoshi', score: 100 });
