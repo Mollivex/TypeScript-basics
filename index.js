1384e55d9e3b9e28079ff9edce709097888dbd37
Alexandr Cocier <licoms1997@gmail.com>
2023-12-27 09:54:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+var age = 30;
+
+console.log(age);
---
+++ index.ts
@@ -0,0 +1,27 @@
+// TYPE BASICS
+
+let age: number = 30;
+let firstName: string = 'Mario';
+let isFictional: boolean;
+
+age = 31;
+
+firstName = 'Luigi';
+isFictional = true;
+
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+
+planet = 'Saturn';
+moons = 145;
+isLarge = true;
+
+// null & undefined
+let something: null;
+let anotherThing: undefined;
+
+something = null;
+anotherThing = undefined;
+
+
