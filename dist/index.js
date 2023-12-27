0a67b78c0e1bb0f3be7ee9a3ebd976bf6270bdf0
Alexandr Cocier <licoms1997@gmail.com>
2023-12-27 11:18:42 +0200

    Basics of Arrays & Object Literals

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log('hello, ninjas!!!');
+//--------
+// arrays
+//--------
+let names = ['Mario', 'Luigi', 'Peach'];
+let ages = [25, 28, 24];
+names.push('true');
+ages.push(35);
+//---------------------------
+// type inference with arrays
+//---------------------------
+let fruits = ['apples', 'mangos', 'bananas', 'pears'];
+fruits.push('peaches');
+const f = fruits[3];
+let things = [1, true, 'hello'];
+const t = things[0];
+//-----------------
+// objects literals
+//-----------------
+let user = {
+    firstName: 'Mario',
+    age: 24,
+    id: 181,
+    isFictional: true
+};
+user.firstName = 'peach';
+user.id = 2;
+//------------------------------------
+// type inference with object literals
+//------------------------------------
+let person = {
+    name: 'Luigi',
+    score: 35
+};
+person.name = 'Bowser';
+person.score = 24;
+const score = person.score;
